543615525a65f09976f8d3597a3fa988c951d766
Robin Blondin <robin.blondin@protonmail.com>
2024-03-19 14:47:14 +0100

    Added fetch from API and create cards into index

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
-specifikKnapp.addEventListener;
+async function fetchProducts() {
+    try {
+        const response = await fetch('https://fakestoreapi.com/products/')
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error + "Fetch has failed")
+    }
+}
 
+const productList = await fetchProducts()
+
+async function createCardTemplate() {
+    try {
+        const response = await fetch('./card.html');
+        const template = await response.text();
+        return template;
+    } catch (error) {
+        console.error("Failed to fetch card template:", error);
+        return null;
+    }
+}
+
+async function createCard() {
+    const cardTemplate = await createCardTemplate();
+    if (!cardTemplate) {
+        console.error("Card template not available");
+        return;
+    }
+
+    productList.forEach(product => {
+        const card = document.createElement('div');
+        card.innerHTML = cardTemplate;
+
+        card.querySelector('.card-img-top').src = product.image;
+        card.querySelector('.card-title').textContent = product.title;
+        card.querySelector('.card-price').textContent = product.price;
+
+        document.getElementById("container-cards").appendChild(card);
+    });
+}
+
+createCard();
