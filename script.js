7b3e236b1a5ed5784884764916773effdbf8c1f5
tarretarre <143603581+tarretarre@users.noreply.github.com>
2024-03-20 15:36:24 +0100

    Co-authored-by: Michael Speiner <MSpeiner@users.noreply.github.com> Co-authored-by: Robin Blondin <RobinBlondin@users.noreply.github.com> Co-authored-by: filipdetterfelt <filipdetterfelt@users.noreply.github.com> Co-authored-by: NadiaNazari1987 <NadiaNazari1987@users.noreply.github.com>

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ async function createCardTemplate() {
     try {
         const response = await fetch('./card.html');
         const template = await response.text();
+        console.log('template created')
         return template;
     } catch (error) {
         console.error("Failed to fetch card template:", error);
@@ -61,10 +62,54 @@ async function createCard() {
 
         card.querySelector('.card-img-top').src = product.image;
         card.querySelector('.card-title').textContent = product.title;
-        card.querySelector('.card-price').textContent = product.price;
+        card.querySelector('.card-price').textContent = "$" + product.price;
+        
+        const cardButton = card.querySelector('.card-button');
+        cardButton.setAttribute('buttonid', product.id);
+
+        cardButton.addEventListener('click', () => {
+            console.log('hello3')
+            const productForm = document.querySelector('.product-info');
+            productForm.style.display = "block";
+            productInfo(product.image, 
+                product.title, 
+                product.description, 
+                "$" + product.price, 
+                product.id);
+            console.log(cardButton.getAttribute('buttonid'))
+        });
 
         document.getElementById("container-cards").appendChild(card.firstChild);
     });
+
+    
+    console.log('cards created')
 }
 
 createCard();
+
+function productInfo(image, title, desc, price, productId) {
+    const productInfo = document.querySelector('.product-info');
+    productInfo.querySelector('.card-img-top').src = image;
+    productInfo.querySelector('.info-title').textContent = title;
+    productInfo.querySelector('.info-desc').textContent = desc;
+    productInfo.querySelector('.info-price').textContent = price;
+
+    const infoButton = productInfo.querySelector('.info-button');
+    infoButton.setAttribute('buttonid', productId);
+    infoButton.addEventListener('click', () => localStorage.setItem('productId',productId) );
+    
+}
+
+
+function createCustomerList(){
+    const pictureOfProduct = productInfo.querySelector('.card-img-top').src = image;
+    const titleOfProduct = productInfo.querySelector('.info-title').textContent = title;
+    const descOfProduct = productInfo.querySelector('.info-desc').textContent = desc;
+    const priceOfProduct = productInfo.querySelector('.info-price').textContent = price;
+    const listKund = [pictureOfProduct, titleOfProduct, descOfProduct, priceOfProduct]
+}
+
+function createProductList(image, title, desc, price, productId){
+    const listProduct = [image, title, desc, price, productId]
+}
